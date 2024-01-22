d26142e58781b0e2d42a87df53ec7332c40361df
aimanshayan <157368827+aimanshayan@users.noreply.github.com>
2024-01-22 16:38:07 +0500

    Add files via upload

---
+++ aimanshayan.py
@@ -0,0 +1,16 @@
+print("calculator")
+num1=int(input("enter the value: "))
+num2=int(input("enter the value: "))
+print("press '+' for additon\npress '-' for subtraction\npress '*' for multiplication\npress '/' for division")
+choice=input("enter your choice +-*/: ")
+if choice=='+':
+    print(num1+num2)
+elif choice=='-':
+    print(num1num2)
+elif choice=='*':
+    print(num1*num2)
+elif choice=='/':
+    print(num1/num2)
+else:
+    print("invalid choice")
+    
